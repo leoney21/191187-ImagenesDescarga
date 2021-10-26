72ccfd05b08acb1dc899c8e0a566724b03bd51bc
leoney21 <92281116+leoney21@users.noreply.github.com>
2021-10-25 23:45:13 -0500

    Add files via upload

---
+++ multiproceso.py
@@ -0,0 +1,33 @@
+from imgurpython import ImgurClient
+from multiprocessing import Pool #Pool nos permite una cantidad de hilos que controlomas cantidad y tiempo de vida
+import os
+import urllib.request
+import timeit
+ 
+secreto_cliente = "5f8c3cce299db5e26a2eb96b0b7809a82805c9ad"
+id_cliente = "bfa0e227a1c5643"
+cliente = ImgurClient(id_cliente, secreto_cliente)
+ 
+ 
+def descarga_url_img(link):
+   print(link)
+   # Con esto ya podemos obtener el corte de la url imagen
+   nombre_img = link.split("/")[3]
+   formato_img = nombre_img.split(".")[1]
+   nombre_img = nombre_img.split(".")[0]
+   print(nombre_img, formato_img)
+   url_local = "./images5/{}.{}"
+   #Guardar nne local las imagenes
+   urllib.request.urlretrieve(link, url_local.format(nombre_img, formato_img))
+ 
+ 
+def main():
+   id_album = "bUaCfoz"
+   imagenes = [img.link for img in cliente.get_album_images(id_album)]
+   cantidad_hilos = len(imagenes)
+
+   with Pool(processes = cantidad_hilos) as pool:
+       pool.map(descarga_url_img, imagenes)
+ 
+if __name__ == "__main__":
+   print("Tiempo de descarga {}".format(timeit.Timer(main).timeit(number=1)))
